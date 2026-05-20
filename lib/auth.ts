5bda064e211db834073410bf8c6ed3203de7a1f1
Jessie Misner <vowvariety@gmail.com>
2026-05-20 12:18:57 -0500

    Fix TypeScript JWT_SECRET type assertion

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 if (!JWT_SECRET) {
   throw new Error('JWT_SECRET environment variable is not set');
 }
